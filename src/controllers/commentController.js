b19871685db2b0f5670278c3176e1b9075d74096
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-05-11 23:50:12 +0530

    update and delete comment controllers

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -25,11 +25,32 @@ export const addComment = async (req, res) => {
 export const getComments = async (req, res) => {
     try {
         const comments = await Comments.find({ blogId: req.params.id }).populate('userId', '-password -email -fullName -createdAt -updatedAt')
+            .sort({ createdAt: -1 })
         if (!comments) return res.status(400).json({ message: 'cannot find any comments', success: false })
         return res.status(200).json({ success: true, comments });
     } catch (error) {
         console.log('internal server error in fetching comments', error);
         return res.status(500).json({ success: false, message: 'server error in getting all comments ' })
 
+    }
+}
+export const deleteComment = async (req, res) => {
+    try {
+        await Comments.findByIdAndDelete(_id)
+        return res.status(200).json({ success: true, message: 'comment Deleted' })
+    } catch (error) {
+        console.log('error in deleting comment', error);
+        return res.status(500).json({ message: 'cant delete due to internal server error' })
+    }
+}
+
+export const updateComment = async (req, res) => {
+    try {
+        const response = Comments.findOneAndUpdate(commentId, { text: newText });
+        if (!response) return res.status(400).json({ message: 'cannot find id' })
+        return res.status(200).json({ message: 'success' }, response)
+    } catch (error) {
+        console.log('error in updating comments', error);
+        return res.status(500).json({ message: 'internal server error' })
     }
 }
