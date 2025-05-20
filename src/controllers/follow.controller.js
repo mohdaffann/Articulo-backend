8dd3ce0ba2a8f46216a0998004353b30f424860a
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-05-20 21:27:32 +0530

    optimizing follow controller logic

--- src/controllers/follow.controller.js
+++ src/controllers/follow.controller.js
@@ -1,6 +1,8 @@
 import { Follow } from "../models/follow.model.js";
 import { User } from "../models/user.model.js";
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
+
+const isValidObjectId = (id) => mongoose.Types.ObjectId.isValid(id);
 export const followUser = async (req, res) => {
     try {
 
@@ -9,64 +11,45 @@ export const followUser = async (req, res) => {
         const existUser = await User.findById(userID);
         if (!existUser) return res.status(400).json({ message: 'User does not exists' })
         const { id } = req.params;
-        if (!mongoose.Types.ObjectId.isValid(id)) return res.status(400).json({ message: 'not a valid id of user you want to follow' })
-        const providedId = await User.findById(id)
+        if (!isValidObjectId(id)) return res.status(400).json({ message: 'not a valid id of user you want to follow' })
+        const providedId = await User.findById(id).select('-password -createdAt -updatedAt -email -fullName')
         if (!providedId) return res.status(400).json({ message: "Invalid id of user you want to follow" })
-        if (userID === id) return res.status(400).json({ message: 'cannot follow yourself!' })
+        if (userID.toString() === id) return res.status(400).json({ message: 'cannot follow yourself!' })
+        const alreadyFollowing = await Follow.find({ follower: userID, following: id })
+        if (alreadyFollowing.length > 0) return res.status(409).json({ message: 'Already following' })
         const followInstance = new Follow(
             {
                 follower: userID,
                 following: providedId
             })
         await followInstance.save();
-        const currentFollowing = await Follow.find({ follower: userID }).populate("following", 'userName')
-        return res.status(201).json({ message: 'followed successfully', currentFollowing })
+
+        return res.status(201).json({ message: 'followed successfully', user: providedId })
     } catch (error) {
-        if (error.code === 11000)
-            return res.status(409).json({ message: 'user already follows ' })
+        return res.status(500).json({ message: 'internal server error' }, error)
     }
 }
 
-const getFollowingList = async (req, res) => {
+export const getFollowingList = async (req, res) => {
     try {
-        const userId = req.user._id;
-        const { id } = req.params;
-        if (!mongoose.Types.ObjectId.isValid(id)) return res.status(400).json({ message: 'invlid id of user of whos following list yo want to view' })
-        const followingList = await Follow.find({ follower: id }).populate("following", 'userName')
+        const userID = req.params.id
+        if (!isValidObjectId(userID)) return res.status(400).json({ message: 'invlid id of user of whos following list yo want to view' })
+        const followingList = await Follow.find({ follower: userID }).populate("following", 'userName _id profile')
         if (!followingList) return res.status(400).json({ message: 'user doesnt follow anyone' })
         return res.status(200).json({ message: 'list of followers fetched successful', followingList })
     } catch (error) {
         return res.status(500).json({ message: 'server error' })
     }
 }
-const getFollowerList = async (req, res) => {
+export const getFollowerList = async (req, res) => {
     try {
-        const userId = req.user._id;
-        const { id } = req.params;
-        if (!mongoose.Types.ObjectId.isValid(id)) return res.status(400).json({ message: 'invlid id of user of whos following list yo want to view' })
-        const followerList = await Follow.find({ following: id }).populate("follower", 'userName')
+        const userId = req.params.id
+        console.log('inside followers controller', userId);
+
+        if (!isValidObjectId(userId)) return res.status(400).json({ message: 'invlid id of user of whos following list yo want to view' })
+        const followerList = await Follow.find({ following: userId }).populate("follower", 'userName profile')
         if (!followerList) return res.status(400).json({ message: 'user doesnt follow anyone' })
         return res.status(200).json({ message: 'list of followers fetched successful', followerList })
-    } catch (error) {
-        return res.status(500).json({ message: 'server error' })
-    }
-}
-export const getCurrentUserFollowing = async (req, res) => {
-    try {
-        const userId = req.user._id;
-        const getFollowing = await Follow.find({ follower: userId }).populate("following", 'userName')
-        if (!getFollowing) return res.status(400).json({ message: 'user doesnt follow anyone' })
-        return res.status(200).json({ message: 'successful fetch', getFollowing })
-    } catch (error) {
-        return res.status(500).json({ message: 'server error' })
-    }
-}
-export const getCurrentUserFollowers = async (req, res) => {
-    try {
-        const userId = req.user._id;
-        const getFollowers = await Follow.find({ following: userId }).populate("follower", 'userName')
-        if (!getFollowers) return res.status(400).json({ message: 'user doesnt have any followers' })
-        return res.status(200).json({ message: 'successful fetch', getFollowers })
     } catch (error) {
         return res.status(500).json({ message: 'server error' })
     }
